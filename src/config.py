30898cc75f67d2f533749a950d172cbd1361b1ac
John Germing <johngerming8@gmail.com>
2025-03-08 03:38:41 -0800

    Created dictionaries to utilize for machine # configurations and warmup parameters.

--- src/config.py
+++ src/config.py
@@ -6,3 +6,18 @@ Description: This script will store the configuration settings
              for the CNC warmup generator.
 """
 
+# Machine # configuration settings for X and Y axis travel limits.
+MACHINES = {
+    "1": {"X": 762, "Y": 508},
+    "2": {"X": 1016, "Y": 660},
+    "3": {"X": 1270, "Y": 508}
+}
+
+# Machine configuration for spindle RPM, feedrate mm/min, and flood coolant activation.
+DEFAULT_SETTINGS = {
+    "start_rpm": 500,
+    "finish_rpm": 3000,
+    "start_feedrate": 200,
+    "finish_feedrate": 800,
+    "coolant": True
+}
